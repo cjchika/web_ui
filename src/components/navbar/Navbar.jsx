1dc021080c078048ca7f614b0406fce34b0d4370
Chijioke Chika <8pluscreative@gmail.com>
2022-06-19 13:35:38 -0400

    Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
+import logo from '../../assets/logo.svg'
+
 import './navbar.css'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='webui__navbar'>
+      <div className='webui__navbar-links'>
+        <div className='webui__navbar-links_logo'>
+          <img src={logo} alt="logo"/>
+        </div>
+        <div className='webui__navbar-links_container'>
+          <ul>
+            <li><a href='#home'>Home</a></li>
+            <li><a href='#wgpt3'>What is GPT3</a></li>
+            <li><a href='#possibility'>Open AI</a></li>
+            <li><a href='#features'>Case Studies</a></li>
+            <li><a href='#blog'>Library</a></li>
+          </ul>
+        </div>
+      </div>
+      <div className='webui__navbar-sign'>
+        <ul>
+          <li>Sign in</li>
+        </ul>
+        <button type='button'>Sign up</button>
+      </div>
+      <div className='webui__navbar-menu'>
+
+      </div>
+    </div>
   )
 }
 
