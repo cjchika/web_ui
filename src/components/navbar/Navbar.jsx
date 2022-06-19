5ddf01b6c78ea30551e18079d80352e9c65a5d0c
Chijioke Chika <8pluscreative@gmail.com>
2022-06-19 15:35:56 -0400

    Navbar implemented

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,16 @@
 import React, { useState } from 'react'
-import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
+import { RiMenuUnfoldFill, RiCloseLine } from 'react-icons/ri'
 import logo from '../../assets/logo.svg'
 
 import './navbar.css'
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false)
+
+  const toggleHandler = () => {
+    setToggleMenu(toggleMenu => !toggleMenu)
+  }
+
   return (
     <div className='webui__navbar'>
       <div className='webui__navbar-links'>
@@ -27,8 +33,32 @@ const Navbar = () => {
         </ul>
         <button type='button'>Sign up</button>
       </div>
-      <div className='webui__navbar-menu'>
 
+      
+      <div className='webui__navbar-menu'>
+        {toggleMenu 
+        ? <RiCloseLine color='#fff' size={27} onClick={toggleHandler} /> 
+        : <RiMenuUnfoldFill color='#fff' size={27} onClick={toggleHandler} />
+        }
+        {toggleMenu && (
+          <div className='webui__navbar-menu_container scale-up-center'>
+            <div className='webui__navbar-menu_container-links'>
+              <ul>
+                <li><a href='#home'>Home</a></li>
+                <li><a href='#wgpt3'>What is GPT3</a></li>
+                <li><a href='#possibility'>Open AI</a></li>
+                <li><a href='#features'>Case Studies</a></li>
+                <li><a href='#blog'>Library</a></li>
+              </ul>
+              <div className='webui__navbar-menu_container-links-signup'>
+                <ul>
+                  <li>Sign in</li>
+                </ul>
+                <button type='button'>Sign up</button>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   )
