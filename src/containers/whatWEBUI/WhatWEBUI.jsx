1b9c7075d2f95f7ba7d2ba2f9e13beb1f7378f87
Chijioke Chika <8pluscreative@gmail.com>
2022-06-20 15:26:25 -0400

    Feature sec

--- src/containers/whatWEBUI/WhatWEBUI.jsx
+++ src/containers/whatWEBUI/WhatWEBUI.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Feature } from '../../components'
 import './whatwebui.css';
 
 const WhatWEBUI = () => {
   return (
-    <div>WhatWEBUI</div>
+    <div className='gpt3__whatgpt3 section__margin' id='wgpt3'>
+      <div className='gpt3__whatgpt3-feature'>
+        <Feature />
+      </div>
+      <div className='gpt3__whatgpt3-heading'>
+        <h1 className='gradient-text'>The possibilities are beyond your imagination</h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className='gpt3__whatgpt3-container'>
+        <Feature />
+        <Feature />
+        <Feature />
+      </div>
+    </div>
   )
 }
 
